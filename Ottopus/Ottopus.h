eea869283333d5b9b0294417881c87838606fa70
WONDAEHAN <74443021+wondaehan@users.noreply.github.com>
2022-01-10 18:04:20 +0900

    Create Ottopus.h

---
+++ Ottopus/Ottopus.h
@@ -0,0 +1,30 @@
+/* makebot ottopus library
+ * makebot homepage : https://makebot.ml
+ * files : https://github.com/makebot-lab/ottopus-lib
+ * developed by WONDAEHAN.
+ */
+#ifndef Ottopus_h
+#define Ottopus_h
+
+#include <Servo.h>
+#include <Oscillator.h>
+#include <US.h>
+#include "Arduino.h"
+
+class Ottopus {
+  public:
+    void ready();
+    void attachservo(int x);
+    void detachservo(int x);
+    void setangles(int a, int b, int c, int d, int e, int f);
+    float distance();
+    void home();
+    void move(int movenum);
+    void motion(int motionnum);
+    void sound(int soundnum);
+  private:
+    Oscillator servo[6];
+    US us;
+};
+
+#endif
